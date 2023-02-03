a20047070800afb09efa9a051a5fa1833e2cfd5d
RogerFaria <faria.rr@outlook.com>
2023-02-03 12:19:40 -0300

    SendingPrograms

---
+++ ImportCA/ImportCA.cmd
@@ -0,0 +1,37 @@
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------   
+
+certutil -store "ROOT" db37988f6babfbda7281da8c42c2bd2b157c6d8b
+
+if not ERRORLEVEL 0 certutil -addstore "root" ca\rootca.crt
+
+certutil -store "CA" d0f5a592647b54ba37983a5f9a00c6b9e5acbc5f
+
+if not ERRORLEVEL 0 certutil -addstore "CA" ca\subca.crt
